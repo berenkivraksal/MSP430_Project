7eb62dbc266373b3ebe7dacfac7a120c09445631
Ceren Candemir <ceren.candemir.cc@gmail.com>
2025-12-26 15:16:24 +0300

    I wrote the IDLE function and the necessary Yellow, Green, Red, and Blue functions for the IDLE function. I also wrote a Delay and a Dloop function to make each color light up for 2 seconds.

--- main.asm
+++ main.asm
@@ -41,6 +41,46 @@ StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
     bis.b #00110110b, &P2REN    ;I'm opening the button resistor because I'm going to operate the button.
     bis.b #00110110b, &P2OUT    ;1 when no button is pressed. 0 when button is pressed.
 
+IDLE:
+    call #Yellow
+    call #Green
+    call #Red
+    call #Blue
+
+Yellow:
+    bis.b #BIT1, &P1OUT
+    bic.b #00110100b, &P1OUT
+    mov.w #6, r4    ;I'm assigning a value of 6 to r4 to keep the yellow light on for 2 seconds.
+    ret
+
+Green:
+    bis.b #BIT2, &P1OUT
+    bic.b #00110010b, &P1OUT
+    mov.w #6, r4    ;I'm assigning a value of 6 to r4 to keep the green light on for 2 seconds.
+    ret
+
+Red:
+    bis.b #BIT4, &P1OUT
+    bic.b #00100110b, &P1OUT
+    mov.w #6, r4    ;I'm assigning a value of 6 to r4 to keep the red light on for 2 seconds.
+    ret
+
+Blue:
+    bis.b #BIT5, &P1OUT
+    bic.b #00010110b, &P1OUT
+    mov.w #6, r4    ;I'm assigning a value of 6 to r4 to keep the blue light on for 2 seconds.
+    ret
+    jmp Yellow
+
+Delay:
+    mov.w #0xFFFF, r5   ;At first we wanted to do r5 as larger as we can.
+Dloop:
+    dec.w r5
+    jne Dloop
+    dec.w r4
+    jne Delay
+    ret
+
 ;-------------------------------------------------------------------------------
 ; Stack Pointer definition
 ;-------------------------------------------------------------------------------
