f002f32fa419b9b2a3b877df119683d8e8194b50
Ceren Candemir <ceren.candemir.cc@gmail.com>
2025-12-26 16:56:48 +0300

    General Arrangement

--- main.asm
+++ main.asm
@@ -43,40 +43,65 @@ StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
     bis.b #00110110b, &P2REN    ;I'm opening the button resistor because I'm going to operate the button.
     bis.b #00110110b, &P2OUT    ;1 when no button is pressed. 0 when button is pressed.
 
+INIT_IDLE:
+    mov.w #2, r6
+
 IDLE:
     call #Yellow
+    call #Check_Start_Button
+    cmp.w #0, r6
+    jeq START
+
     call #Green
+    call #Check_Start_Button
+    cmp.w #0, r6
+    jeq START
+
     call #Red
+    call #Check_Start_Button
+    cmp.w #0, r6
+    jeq START
+
     call #Blue
+    call #Check_Start_Button
+    cmp.w #0, r6
+    jeq START
+
+    jmp IDLE
+
+Check_Start_Button:
+    bit.b #00000010b, &P2IN     ;Checking if the yellow button is pressed.
+                                ;If pressed &P2IN = BIT1 = 0, Z=1 -> jeq ise r6--
+    jeq Decrease_Start_Counter
+    ret
+
+Decrease_Start_Counter:
+    dec.w r6
+    ret
 
 Yellow:
     bis.b #BIT1, &P1OUT
     bic.b #00110100b, &P1OUT
-    bic.b #01000000b, &P2OUT
-    mov.w #6, r4    ;I'm assigning a value of 6 to r4 to keep the yellow light on for 2 seconds.
+    mov.w #3, r4    ;I'm assigning a value of 3 to r4 to keep the yellow light on for 1 seconds.
     ret
 
 Green:
     bis.b #BIT2, &P1OUT
     bic.b #00110010b, &P1OUT
-    bic.b #01000000b, &P2OUT
-    mov.w #6, r4    ;I'm assigning a value of 6 to r4 to keep the green light on for 2 seconds.
+    mov.w #3, r4    ;I'm assigning a value of 3 to r4 to keep the green light on for 1 seconds.
     ret
 
 Red:
     bis.b #BIT4, &P1OUT
     bic.b #00100110b, &P1OUT
-    bic.b #01000000b, &P2OUT
-    mov.w #6, r4    ;I'm assigning a value of 6 to r4 to keep the red light on for 2 seconds.
+    mov.w #3, r4    ;I'm assigning a value of 3 to r4 to keep the red light on for 1 seconds.
     ret
 
 Blue:
     bis.b #BIT5, &P1OUT
     bic.b #00010110b, &P1OUT
-    bic.b #01000000b, &P2OUT
-    mov.w #6, r4    ;I'm assigning a value of 6 to r4 to keep the blue light on for 2 seconds.
+    mov.w #3, r4    ;I'm assigning a value of 3 to r4 to keep the blue light on for 1 seconds.
     ret
-    jmp Yellow
 
 Delay:
     mov.w #0xFFFF, r5   ;At first we wanted to do r5 as larger as we can.
@@ -85,6 +110,25 @@ Dloop:
     jne Dloop
     dec.w r4
     jne Delay
+    ret
+
+Easter_Egg_Sequence:
+    bis.b #BIT1, &P1OUT    ;Yellow on
+    bic.b #00110100b, &P1OUT
+    mov.w #3, r4
+    call #Delay
+    bis.b #BIT2, &P1OUT    ;Yellow on, Green on
+    bic.b #00110000b, &P1OUT
+    mov.w #3, r4
+    call #Delay
+    bis.b #BIT4, &P1OUT    ;Yellow on, Green on, Red on
+    bic.b #00100000b, &P1OUT
+    mov.w #3, r4
+    call #Delay
+    bis.b #BIT5, &P1OUT    ;All on
+    mov.w #6, r4
+    call #Delay
+    bic.b #00110110b, &P1OUT ;All off
     ret
 
 ;-------------------------------------------------------------------------------
